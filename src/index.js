f7ddd44235ce44c5ea0918f445020797748e13ae
adudley78 <dudleyadam@gmail.com>
2019-06-19 11:14:12 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// valid React component
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
+}
+
+// ES6
+
+// class Welcome extends React.Component {
+//     render() {
+//         return <h1>Hello, {this.props.name}</h1>
+//     }
+// }
+
+const element = <Welcome name="Sara" />
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
